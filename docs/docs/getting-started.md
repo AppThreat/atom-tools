547c540ce3aa0ed29ec0c76dc433771bb22c204d
Aryan Rajoria <aryanrajoria1003@gmail.com>
2025-01-03 00:20:07 -0500

    add atom-tools to documentation
    Signed-off-by: Aryan Rajoria <aryanrajoria1003@gmail.com>

---
+++ docs/docs/getting-started.md
@@ -0,0 +1,8 @@
+---
+sidebar_position: 1
+title: Getting Started
+slug: /
+---
+
+# Getting Started with Atom-tools
+
--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -38,6 +38,7 @@ const config: Config = {
         docs: {
           sidebarPath: './sidebars.ts',
           // Please change this to your repo.
+          routeBasePath: '/',
           // Remove this to remove the "edit this page" links.
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
